482cc791a82386bbf85552d2982bbfa815f75c48
Elie-Elia <elie.elia@mail.mcgill.ca>
2020-02-15 20:25:18 -0500

    Updated UI for profile settings

--- frontend/src/components/profilesettings.js
+++ frontend/src/components/profilesettings.js
@@ -1,37 +1,159 @@
-import React, { Component } from 'react';
-import {Redirect} from 'react-router-dom';
-import axios from 'axios';
-import { Form, Button, ButtonGroup, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+function getAllAttributes(state, form){
+    let keys = Object.keys(state);
+    keys.forEach((key)=>{
+      state[key] = form[key].value;
+    })
+    return state;
+  }
+
+ 
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.primary.main,
+  },
+  form: {
+    width: '100%', 
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function UserProfile() {
+  const classes = useStyles();
 
 
-export default class ProfileSettings extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-
-            <div align="center">
-            <ButtonGroup vertical> 
-            <Button variant="primary" size="lg">
-                Favourites
-            </Button>
-            <Button  onClick={event =>  window.location.href='/userorders'} variant="primary" size="lg">
-                My Order History
-            </Button>
-            <Button variant="primary" size="lg">
-                Addresses
-            </Button>
-
-            <Button variant="primary" size="lg">
-                Wallet
-            </Button>
-
-            <Button onClick={event =>  window.location.href='/profilesettings'} variant="primary" size="lg">
-                Profile Settings
-            </Button>
-            </ButtonGroup>  
-          </div>
-        )
-      }
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <AccountCircleIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          User Profile
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="Street"
+                label="Street"
+                type="Street"
+                id="Street"
+                autoComplete="address"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="City"
+                label="City"
+                type="City"
+                id="City"
+                autoComplete="City"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="Zipcode"
+                label="Zipcode"
+                type="Zipcode"
+                id="Zipcode"
+                autoComplete="Zipcode"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Update Profile
+          </Button>
+          <Grid container justify="flex-end">
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}>
+       
+      </Box>
+    </Container>
+  );
 }
