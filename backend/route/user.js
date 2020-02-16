181fb6cbf8e4a03806ff57039c4746157a3ce096
20001LastOrder <boqi.chen@mail.mcgill.ca>
2020-02-15 19:07:45 -0500

    change extent schema to discriminator (batter inheritance)

--- backend/route/user.js
+++ backend/route/user.js
@@ -31,18 +31,20 @@ router.route('/login').get((req, res) => {
                 res.json({success: true, err: null, role: user.isServer, token});
             });
         } else {
+            console.error(err);
             res.status(400).json(err);
         }
     });
 });
 
-router.route('/signup').post((req, res) => {
-    // console.log(req.body)
-    new User(req.body).save(function(err, doc) {
-        if (err) res.status(400).json(err);
-        else res.status(201).json(doc);
-    });
-    
+router.route('/signup').post(async (req, res) => {
+    try{
+        let user = new User(req.body).save();
+        res.status(201).json(user);
+    }catch(err){
+        
+        res.status(400).json(err);
+    }
 });
 
 router.route('/delete').delete((req, res) => {
