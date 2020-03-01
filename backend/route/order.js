97e2b00aecd0e6e81fdebf36ab1ed5df744d6ba0
Carl-elkhoury <carlelkhoury@hotmail.com>
2020-03-01 15:27:09 -0500

    feature finished

--- backend/route/order.js
+++ backend/route/order.js
@@ -7,7 +7,7 @@ let User = require('../model/user');
 let Order = require('../model/order');
 
 router.route('/create').post(async (req, res) => {
-    if(!req.body.customerId || !req.body.restaurantId){
+    if(!req.body.customerId || !req.body.restaurantId || !req.body.order_items){
         res.status(400).json("bad req");
         return
     }
@@ -18,18 +18,15 @@ router.route('/create').post(async (req, res) => {
 
         if(!rest || !cust) 
             throw "Restaurant or customer does not exist";
-        
         let orderObj = req.body; 
         
         let price = 0; 
         let itemList = [];
-
         for(const itemId of orderObj.order_items){
             let item = await MenuItem.findById(itemId);
             itemList.push(item);
             if(item) price+=item.price;
         }
-
         orderObj.price = price;
 
         let createdOrder = new Order({
@@ -38,10 +35,8 @@ router.route('/create').post(async (req, res) => {
             order_items: itemList,
             price : orderObj.price, 
         });
-
         await linkOrderToRestaurantAndCustomer(createdOrder._id, createdOrder.restaurantId, createdOrder.customerId);
         await createdOrder.save();
-
         res.status(201).json(createdOrder);
     } catch (err) {
         res.status(500).json(err);
@@ -58,5 +53,27 @@ async function linkOrderToRestaurantAndCustomer(orderId, restoId, customerId){
         { new: true, useFindAndModify: false });
 
 }
+
+router.route('/update').post(async (req, res) =>{
+    if(!req.body.orderId || !req.body.status){
+        res.status(400).json("bad re1");
+        return
+    }
+    if(req.body.status != "IN_PROGRESS"  && req.body.status != "COMPLETED") {
+        res.status(400).json("bad req");
+        return;
+    }
+    try {
+        let order = await Order.findById(req.body.orderId);
+        if(!order) 
+            throw "Order does not exist";
+        console.log(order);
+        order.status = req.body.status;
+        order.save()
+        res.status(201).json(order);
+    }catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
