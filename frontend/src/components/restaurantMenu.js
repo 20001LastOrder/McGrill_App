4bc11373107dc12f89e29d17e3ea9fdab1e7a7c5
YuxinZhou2 <yuxinzhou2@mail.mcgill.ca>
2020-03-01 19:20:37 -0500

    #47 added ui components that match backend api

--- frontend/src/components/restaurantMenu.js
+++ frontend/src/components/restaurantMenu.js
@@ -1,46 +1,83 @@
 import React, { Component, Fragment } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { TextField, Typography, TextareaAutosize} from '@material-ui/core';
+import { TextField, Typography, Button} from '@material-ui/core';
 import color from '@material-ui/core/colors/amber';
+import axios from 'axios';
+
 
 class restaurantMenu extends Component {
-    render() { 
+    constructor(props) {
+        super(props);
+
+        this.state={
+            name:'',
+            description:'',
+            price:'',
+            stock:'',
+            food_type:''
+        }
+    }
+
+    changeHandler = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    submitHandler = (e) => {
+        console.log(this.state);
+    }
+    render() {
+        const {food_type,name,price,stock,description} = this.state 
         return (
             <Fragment>
           <Typography variant="h3" style={{textAlign:'center'}}>Menu</Typography>
-          <form>
+          <form onSubmit={this.submitHandler}>
               <div style={{position: 'absolute', alignContent:'center'}}>
             <TextField
-            id="menuCategory"
+            id="food_type"
             label="Category"
-            defaultValue=""
+            value={food_type}
             helperText="(i.e Appetizers, Soups, etc)"
             variant="outlined"
             style={{marginTop:'20px',position: 'relative'}}
+            onChange={this.changeHandler}
             /> <br></br>
             <TextField
-            id="menuItem"
+            id="name"
             label="Item"
-            defaultValue=""
+            value={name}
             helperText=""
             variant="outlined"
             style={{marginTop:'20px'}}
+            onChange={this.changeHandler}
             /> <br></br>
              <TextField
-            id="itemPrice"
+            id="price"
             label="Price"
-            defaultValue=""
+            value={price}
             helperText=""
             variant="outlined"
             style={{marginTop:'20px'}}
+            onChange={this.changeHandler}
+            /><br></br>
+             <TextField
+            id="stock"
+            label="Stock"
+            value={stock}
+            helperText=""
+            variant="outlined"
+            style={{marginTop:'20px'}}
+            onChange={this.changeHandler}
             /><br></br>
             <textarea 
+            id="description"
+            value={description}
             aria-label="minimum height" 
             placeholder="Enter descrition of the item." 
             style={{resize: 'none', width:'100%', height:'100px',marginTop:'20px'}}
+            onChange={this.changeHandler}
             /><br></br>
-            </div>
-           
+            <Button variant="contained" color="primary" type='submit'>Submit</Button>
+            </div> 
         </form>
         </Fragment>
     );
