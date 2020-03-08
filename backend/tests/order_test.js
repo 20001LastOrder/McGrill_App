2a8d34a56dab72055a25c12092303f9889e95f69
preyansh98 <preyansh98@gmail.com>
2020-03-08 10:35:00 -0400

    add tests

--- backend/tests/order_test.js
+++ backend/tests/order_test.js
@@ -2,13 +2,13 @@ const request = require('supertest');
 const server = require('../server');
 
 const consumer = {
-	"name":"hanzawa",
-	"email":"hanzawa.naoki@tcb.com",
-	"password":"legalhigh",
-	"address":{
-        "street":"87 Kakudacho Kita Ward",
-	    "city":"Osaka",
-        "zip":"PF3X4C"
+    "name": "hanzawa",
+    "email": "hanzawa.naoki@tcb.com",
+    "password": "legalhigh",
+    "address": {
+        "street": "87 Kakudacho Kita Ward",
+        "city": "Osaka",
+        "zip": "PF3X4C"
     }
 };
 
@@ -17,25 +17,25 @@ const owner_and_restaurant = {
         "name": "hattori",
         "email": "hattori@gmail.com",
         "password": "sakaimasato",
-        "address":{
-            "street":"boom",
-            "city":"zoom",
-            "zip":"HHHHHH"
+        "address": {
+            "street": "boom",
+            "city": "zoom",
+            "zip": "HHHHHH"
         }
     },
     restaurant: {
-        "name":"La Butte Boisee",
-        "address":{
-            "street":"6 Chome 19 6",
-            "city":"Tokyo",
-            "zip":"JM379P"
+        "name": "La Butte Boisee",
+        "address": {
+            "street": "6 Chome 19 6",
+            "city": "Tokyo",
+            "zip": "JM379P"
         }
     }
 };
 
 const sample_menu_item = {
-    "name" : "test_menu",
-    "description" : "test_desc",
+    "name": "test_menu",
+    "description": "test_desc",
     "price": 123,
     "sold_out": false,
     "stock": 12,
@@ -66,8 +66,8 @@ async function createRestaurantAndUserAndFoodAndOrder() {
     obj.token = ownerlogin.body.token;
 
     const food = await request(server)
-        .post('/menu/item/create?restaurantId='+obj.restaurant_id)
-        .set('Authorization',`Bearer ${obj.token}`)
+        .post('/menu/item/create?restaurantId=' + obj.restaurant_id)
+        .set('Authorization', `Bearer ${obj.token}`)
         .set('email', owner_and_restaurant.owner.email)
         .type("json")
         .send(sample_menu_item);
@@ -75,9 +75,9 @@ async function createRestaurantAndUserAndFoodAndOrder() {
     obj.food_id = food.body.menuitems[0];
 
     let make_order = {
-        "customerId":obj.consumer_id,
-        "restaurantId":obj.restaurant_id,
-        "order_items":[obj.food_id]
+        "customerId": obj.consumer_id,
+        "restaurantId": obj.restaurant_id,
+        "order_items": [obj.food_id]
     };
 
     let order = await request(server)
@@ -85,6 +85,7 @@ async function createRestaurantAndUserAndFoodAndOrder() {
         .type("json")
         .send(make_order);
     expect(order.statusCode).toEqual(201);
+    obj.order = order;
     obj.order_id = order.body._id;
 
     return obj;
@@ -105,5 +106,51 @@ module.exports = () => {
             expect(update.statusCode).toEqual(201);
             expect(update.body.status).toEqual(toUpdate.status);
         });
+    });
+
+    describe('Get /restaurant/getCurrentOrders', () => {
+        it('should return current orders of the specified restaurant', async () => {
+            let obj = await createRestaurantAndUserAndFoodAndOrder();
+
+            const res = await request(server)
+                .get('/restaurant/getCurrentOrders')
+                .set('Authorization', `Bearer ${obj.token}`)
+                .query({ 'restaurantId': obj.restaurant_id });
+
+            expect(res.statusCode).toEqual(200);
+            expect(res.body).toContain(obj.order);
+
+            for (let i = 0; i < res.body.length; i++)
+                expect(res.body[i].status).toBe("IN_PROGRESS");
+        })
+    });
+
+    describe('Get /restaurant/getPastOrders', () => {
+        it('should return past orders of the specified restaurant', async () => {
+            //update order first
+            let obj = await createRestaurantAndUserAndFoodAndOrder();
+            let toUpdate = {
+                orderId: obj.order_id,
+                status: "COMPLETE"
+            }
+            let update = await request(server)
+                .post("/order/update")
+                .type("json")
+                .send(toUpdate);
+            expect(update.statusCode).toEqual(201);
+            expect(update.body.status).toEqual(toUpdate.status);
+
+
+            const res = await request(server)
+                .get('/restaurant/getPastOrders')
+                .set('Authorization', `Bearer ${obj.token}`)
+                .query({ 'restaurantId': obj.restaurant_id });
+
+            expect(res.statusCode).toEqual(200);
+            expect(res.body).toContain(obj.order);
+
+            for (let i = 0; i < res.body.length; i++)
+                expect(res.body[i].status).toBe("COMPLETED");
+        })
     });
 };
