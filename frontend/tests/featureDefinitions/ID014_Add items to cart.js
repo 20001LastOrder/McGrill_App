e4549016a8561605c69c370b79bc46ff51340c6f
Chris Li <chris1e686@gmail.com>
2020-03-08 23:30:00 -0400

    create gerkin def

--- frontend/tests/featureDefinitions/ID014_Add items to cart.js
+++ frontend/tests/featureDefinitions/ID014_Add items to cart.js
@@ -1,35 +1,29 @@
 const {client} = require('nightwatch-api');
 const {Given, Then, When} = require('cucumber');
 Given(/^the system is on the page of Tim Hortons and logged in as Kurtis$/, async()=> {
-    
-});
-
-When(/^Kurtis add a double double into the cart$/, async()=> {
-    
-});
-
-When(/^Kurtis add two crispy chicken wraps into the cart$/, async()=> {
-    
-});
-
-When(/^Kurtis add one crispy chicken wrap and two grill chicken wrap into cart$/, async()=> {
-    
-});
-
-Then(/^one double double should be listed in the cart$/, async()=> {
-    
-});
-
-Then(/^two crispy chicken wraps are listed in the cart$/, async()=> {
-    
-});
-
-Then(/^one crispy chicken wrap and two grill chicken wrap should be listed in the cart$/, async()=> {
-    
+    await client.url('http://localhost:3000/user/menu').waitForElementVisible('body', 2000);
 });
 
 Given(/^Tim Hortons provides following items$/, async()=> {
     
 });
+
+When(/^Kurtis add a burger into the cart$/, async()=> {
+    
+});
+
+When(/^Kurtis add two fries into the cart$/, async()=> {
+    
+});
+
+Then(/^one burger should be listed in the cart$/, async()=> {
+    
+});
+
+Then(/^two fries are listed in the cart$/, async()=> {
+    
+});
+
+
 
 //ignore error case
